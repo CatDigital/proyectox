ee7bef39c641792d5bcfb1d83f713b26056f35a2
diegoinf2024 <diegoinf2024@gmail.com>
2025-04-15 23:13:23 -0400

    mi primer cambio

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Mi Intranet</title>
+    <title>Mi oooooooooooo</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Bootstrap CSS -->
