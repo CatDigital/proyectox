f24cea0114fde2c48e93a693b8c7c24dbb229f73
catdigital <catdigital.web@gmail.com>
2025-04-17 00:17:33 -0400

    cambio de atributos

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'nombre_user',
         'email',
         'password',
     ];
@@ -30,7 +30,7 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
+        'token',
     ];
 
     /**
